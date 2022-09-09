a5ca45eee80a2f8be442c045476060669184be62
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-09 14:59:45 -0500

    Render experience component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Education from './components/Education';
+import Experience from './components/Experience';
 import Info from './components/Info'
 function App() {
   return (
     <div className="App">
       <Info />
       <Education />
+      <Experience />
     </div>
   );
 }
