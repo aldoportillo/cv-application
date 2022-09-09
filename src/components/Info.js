dd36086397889fef59d7b503ad0fcf30dea63c97
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-09 14:43:44 -0500

    Add handleChange function

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,65 @@
 import React from 'react'
 
 export default function Info () {
+
+    const [info, setInfo] = React.useState({
+        name: "",
+        email: "",
+        phone: "",
+    })
+
+    console.log(info)
+    function handleChange (event) {
+        
+        const {name, value} = event.target
+
+        setInfo(prevInfo => {
+            return{
+                ...prevInfo,
+                [name]: value,
+            }
+        })
+
+        //console.log(value)
+    }
+    function handleSubmit(){
+
+    }
     return(
-        <h1>Info works</h1>
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                
+                <input 
+                    type="text" 
+                    placeholder="Name"
+                    onChange={handleChange}
+                    name="name"
+                />
+                
+                <input 
+                    type="email" 
+                    placeholder="Email address"
+                    className="form--input"
+                    onChange={handleChange}
+                    name="email"
+                />
+
+                <input 
+                    type="number" 
+                    placeholder="Phone number"
+                    onChange={handleChange}
+                    name="phone"
+                />
+
+                <button 
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+
+
     )
     
 }
