dc486f414cc823b6a747f255add3fcb7c512a9f6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-13 14:44:40 -0500

    Render Header component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import CurriculumVitae from './components/CurriculumVitae';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Header from './components/header';
 import Info from './components/Info'
 function App() {
   return (
     <div className="App">
+      <Header />
       <Info />
       <Education />
       <Experience />
-      <CurriculumVitae />
+      <CurriculumVitae/>
     </div>
   );
 }
