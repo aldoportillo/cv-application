fcc2083df88915bd0343a212b7241f6028aa2cd8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-13 15:25:53 -0500

    Remove imports for deleted components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import CurriculumVitae from './components/CurriculumVitae';
-import Education from './components/Education';
-import Experience from './components/Experience';
+
 import Header from './components/header';
 import Form from './components/Form'
-import Info from './components/Info'
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
-      <CurriculumVitae/>
     </div>
   );
 }
